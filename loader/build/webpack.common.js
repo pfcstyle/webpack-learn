eac760da3b134701f55c915f191386bb3fe8a8de
yawe9101 <ywang@esri.com>
2020-04-19 20:36:35 +0800

    add loader

---
+++ loader/build/webpack.common.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const devConfig = require('./webpack.dev.js');
+const prodConfig = require('./webpack.prod.js');
+const commonConfig = {
+	entry: {
+		main: './src/index.js',
+	},
+	module: {
+		rules: [{ 
+			test: /\.js$/, 
+			exclude: /node_modules/,
+			use: [{
+				loader: 'babel-loader'
+			}, {
+				loader: 'imports-loader?this=>window'
+			}]
+		}, {
+			test: /\.(jpg|png|gif)$/,//和file-loader功能类似，都是支持静态文件的load
+			use: {
+				loader: 'url-loader',
+				options: {
+					name: '[name]_[hash].[ext]',
+					outputPath: 'images/',
+					limit: 10240//当静态文件大于10240B时，会打包到images文件，否则会直接以数据流方式打包到js中，节省request
+				}
+			} 
+		}, {
+			test: /\.(eot|ttf|svg)$/,
+			use: {
+				loader: 'file-loader'
+			} 
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}), 
+		new CleanWebpackPlugin(['dist'], {
+			root: path.resolve(__dirname, '../')
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			_join: ['lodash', 'join']
+		}),
+	],
+	optimization: {
+		runtimeChunk: {
+			name: 'runtime'
+		},
+		usedExports: true,
+		splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+      	vendors: {
+      		test: /[\\/]node_modules[\\/]/,
+      		priority: -10,
+      		name: 'vendors',
+      	}
+      }
+    }
+	},
+	performance: false,
+	output: {
+		path: path.resolve(__dirname, '../dist')
+	}
+}
+
+module.exports = (env) => {
+	if(env && env.production) {
+		return merge(commonConfig, prodConfig);
+	}else {
+		return merge(commonConfig, devConfig);
+	}
+}
+
